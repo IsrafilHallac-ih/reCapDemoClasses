92680f313dcbd4a1a69daa82a82e4d16c303a27c
IsrafilHallac-ih <83336393+IsrafilHallac-ih@users.noreply.github.com>
2021-05-06 21:41:21 +0300

    reCap

---
+++ reCapDemoClasses/src/reCapDemoClasses/FourOperations.java
@@ -0,0 +1,17 @@
+package reCapDemoClasses;
+
+public class FourOperations {
+	public int Addition(int number1,int number2) {
+		return number1+number2;
+	}
+	public int ExtractionProcess(int number1,int number2) {
+		return number1-number2;
+	}
+	public int Multiplication(int number1,int number2) {
+		return number1*number2;
+	}
+	public int Division(int number1,int number2) {
+		return number1/number2;
+	}
+
+}
---
+++ reCapDemoClasses/src/reCapDemoClasses/Main.java
@@ -0,0 +1,14 @@
+package reCapDemoClasses;
+
+public class Main {
+
+	public static void main(String[] args) {
+	FourOperations fourOperations=new FourOperations();
+	int result =fourOperations.Division(13,3 );
+	System.out.println(result);
+	
+	
+
+	}
+
+}
